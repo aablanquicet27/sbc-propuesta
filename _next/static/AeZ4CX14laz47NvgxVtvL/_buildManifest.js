2833e507966e176035c8ac4e810944dc25971a27
aablanquicet27 <aablanquicet@gmail.com>
2026-07-31 01:07:07 +0000

    Reorden: el recorrido del celular del cliente queda inmediatamente despues del hero

---
+++ _next/static/AeZ4CX14laz47NvgxVtvL/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sbc-propuesta//_next/:path+",
+        "destination": "/sbc-propuesta/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
